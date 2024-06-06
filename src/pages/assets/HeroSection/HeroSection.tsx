c3ce6fd629ba1c0656d180b45c565403bc490d7f
Miles0908 <claudioxxmilia@yahoo.it>
2024-06-06 18:21:10 +0200

    Feat: Create all  the mid hero section , need to be finished, thank you claudio

--- src/pages/assets/HeroSection/HeroSection.tsx
+++ src/pages/assets/HeroSection/HeroSection.tsx
@@ -1,14 +1,12 @@
 import styles from "../HeroSection/HeroSection.module.scss"
-
-import content from '../../../../public/content.json';
 import HeroCard from "../HeroCard/HeroCard";
-
-// const { heroSection } = content;
+import MidHeroCard from "../MidHeroCard/MidHeroCard";
 
 const HeroSection: React.FC = () => {
     return (
         <div className={styles.HeroSectionWrapper}>
            <HeroCard/>
+           <MidHeroCard/>
         </div>
     )
 }
